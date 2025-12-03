fecd90b4b70debc030979170e55b799e655d81d5
Andreas Kocman <andreas.kocman@gmail.com>
2025-12-03 22:34:01 +0100

    Day 3 - Lobby Part 1

--- src/main/kotlin/aoc25/dec03/LobbyPart1.kt
+++ src/main/kotlin/aoc25/dec03/LobbyPart1.kt
@@ -11,25 +11,25 @@ fun main() {
 
 fun solveLobby(input: List<String>, numberOfBatteries: Int) =
     input.asSequence()
-        .map { dissolveBatteries(it) }
-        .map { identifyMaximumJoltageGeneric(it, numberOfBatteries)
+        .map { toIntList(it) }
+        .map { identifyMaximumJoltage(it, numberOfBatteries)
             .also { println(it.toBigDecimal().toPlainString()) } }
         .reduce { a, b -> a + b }
 
-private fun dissolveBatteries(input: String) =
+private fun toIntList(input: String) =
     input.split("")
         .filter { it.isNotEmpty() }
         .map { it.toInt() }
         .toList()
 
-private fun identifyMaximumJoltageGeneric(batteries: List<Int>, numberOfRemainingBatteries: Int): Double {
+private fun identifyMaximumJoltage(batteries: List<Int>, numberOfRemainingBatteries: Int): Double {
     if (numberOfRemainingBatteries == 0) {
         return 0.0
     }
 
     val element = batteries.subList(0, batteries.size - numberOfRemainingBatteries + 1).max()
     val remainingBatteries = batteries.subList(batteries.indexOf(element) + 1, batteries.size)
-    val otherElements = identifyMaximumJoltageGeneric(remainingBatteries, numberOfRemainingBatteries - 1)
+    val otherElements = identifyMaximumJoltage(remainingBatteries, numberOfRemainingBatteries - 1)
     val result = (element * 10.0.pow(numberOfRemainingBatteries.toDouble() - 1)) + otherElements
     return result
 }
