fc3439a356e331591c214ace97744c60a193d7a2
Andreas Kocman <andreas.kocman@gmail.com>
2025-12-07 20:13:25 +0100

    Day 6 - Trash Compactor Part 1

---
+++ src/main/kotlin/aoc25/dec06/TrashCompactorPart1.kt
@@ -0,0 +1,56 @@
+package aoc25.dec06
+
+import Parser
+
+fun main() {
+    val input = Parser().readFile("dec06/trash-compactor-input.txt");
+    val parsedInput = parseInput(input)
+    solve(parsedInput)
+        .also { println("Result: $it") }
+}
+
+fun solve(parsedInput: List<Calculation>) =
+    parsedInput.sumOf { calculation ->
+        when (calculation.operator) {
+            Operator.PLUS -> calculation.operands.reduce { acc, v -> acc + v }
+            Operator.MULTIPLY -> calculation.operands.reduce { acc, v -> acc * v }
+        }
+    }
+
+
+fun parseInput(input: List<String>): List<Calculation> {
+    val operands = input.subList(0, input.size - 1)
+    val operations = input[input.size - 1]
+        .split(" ").asSequence()
+        .map { it.trim() }
+        .filter { it.isNotBlank() }
+        .toList()
+
+    val operandsAsNumbers = operands.asSequence()
+        .map {
+            it.split(" ")
+                .map { string -> string.trim() }
+                .filter { it.isNotBlank() }
+                .map { string -> string.toLong() }
+        }
+        .toList()
+
+    return operandsAsNumbers[0].asSequence().withIndex()
+        .map { (index, element) ->
+            operandsAsNumbers.asSequence()
+                .map { list -> list[index] }
+                .toList()
+        }.withIndex()
+        .map { (index, element) ->
+            when (operations[index]) {
+                "+" -> Calculation(element, Operator.PLUS)
+                "*" -> Calculation(element, Operator.MULTIPLY)
+                else -> throw IllegalStateException("Unreachable state")
+            }
+        }
+        .toList()
+}
+
+data class Calculation(val operands: List<Long>, val operator: Operator) {}
+
+enum class Operator { PLUS, MULTIPLY }
